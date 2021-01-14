c6c27a94bc007d2aec550f880ac602e14b7ecf66
VishalChoudhary <54886683+VishalChoudhary@users.noreply.github.com>
2021-01-14 19:05:09 +0530

    Update QuickSort.cpp

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,38 +1,37 @@
 #include<iostream>
 using namespace std;
-void MinMax(int a[],int i,int j, int& max,int& min)
-{
-	if(i==j){
-		max=min=a[i];
+void quicksort(int a[],int first,int last){
+	int i,j,pivot,temp;
+	if(first<last){
+		pivot=first;
+		i=first;
+		j=last;
+		while(i<j){
+			while(a[i]<=a[pivot]&&i<last)
+			i++;
+			while(a[j]>a[pivot])
+			j--;
+			if(i<j){
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+		temp=a[pivot];
+		a[pivot]=a[j];
+		a[j]=temp;
+		quicksort(a,first,j-1);
+		quicksort(a,j+1,last);
 	}
-	else if(i==j-1){
-		if(a[i]<a[j]){
-		max=a[j];
-		min=a[i];
-	   }
-	else
-	max=a[i];
-	min=a[j];
-	}
-	else{
-		int mid,min1,max1;
-		mid=(i+j)/2;
-		MinMax(a,i,mid,max,min);
-		MinMax(a,mid+1,j,max1,min1);
-		if(max<max1)
-		max=max1;
-		if(min>min1)
-		min=min1;
-	}
-	
 }
-int main()
-{
-	int a[]={22,18,-5,-8,15,60,30,31,47};
-	int n=sizeof(a)/sizeof(a[0]);
-	int max = INT_MAX, min = INT_MIN;
-	MinMax(a,0,n-1,max,min);
-	cout<<"Max= "<<max;
-	cout<<"\nMin= "<<min;
+int main(){
+	int i,n;
+	int a[]={10, 7, 8, 9, 1, 5};
+	n=sizeof(a)/sizeof(a[0]);
+	quicksort(a,0,n-1);
+	cout<<"Quick Sorted Array: ";
+	for(i=0;i<n;i++){
+		cout<<a[i]<<"\t";
+	}
 	return 0;
 }
